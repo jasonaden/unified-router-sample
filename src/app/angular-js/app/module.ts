3fe8e2ee1a8217d15c577f75a8502eab06903a18
Jason Aden <jasonaden@google.com>
2019-01-18 11:57:22 -0800

    Update look of route rendered by AngularJS

--- src/app/angular-js/app/module.ts
+++ src/app/angular-js/app/module.ts
@@ -4,11 +4,11 @@ import { ROUTER_UPGRADE_MODULE } from 'src/app/upgrade/angular-js/module';
 
 export const appModule = angular.module('app', [ROUTER_UPGRADE_MODULE]);
 
-appModule.config(function($routeProvider: ng.route.IRouteProvider, $locationProvider) { 
+appModule.config(function($routeProvider: ng.route.IRouteProvider, $locationProvider) {
   $locationProvider.html5Mode(true);
 
   $routeProvider.when('/dashboard', {
-    template: '<p>angular-js</p>'
+    template: '<h1>Route rendered by AngularJS</h1>'
   })
   .otherwise({
     template: 'blank'
